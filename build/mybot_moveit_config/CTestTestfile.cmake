f1c61857e483b4172605afcf60ca298d651c4c3d
Triang1 <1516501282@qq.com>
2024-12-29 21:27:39 +0800

    Remove build, install, and log directories

--- build/mybot_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/mybot/mybot_ws/src/mybot_moveit_config
-# Build directory: /home/robot/mybot/mybot_ws/build/mybot_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/mybot_moveit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/robot/mybot/mybot_ws/install/mybot_description/share/mybot_description/package.sh"
